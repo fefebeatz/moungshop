13ebb8de44f30e07c8226146142b3717c2ad99a1
Ferreol <fefeferreol242@gmail.com>
2025-09-07 16:09:41 +0100

    Modification des erreurs

--- app/(store)/commandes/page.tsx
+++ app/(store)/commandes/page.tsx
@@ -1,152 +1,149 @@
-import { imageUrl } from '@/lib/imageUrl';
-import { Order } from '@/sanity.types';
-import { getMyOrders } from '@/sanity/lib/orders/getMyOrders';
-import { auth } from '@clerk/nextjs/server';
-import Image from 'next/image';
-import { redirect } from 'next/navigation';
+import { imageUrl } from '@/lib/imageUrl'
+import { getMyOrders } from '@/sanity/lib/orders/getMyOrders'
+import { auth } from '@clerk/nextjs/server'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
 async function Orders() {
-    const { userId } = await auth();
-    
-    if(!userId)
-    {
-      return redirect('/');
-    }
+  const { userId } = await auth()
 
-    const orders = await getMyOrders(userId); 
-    let totalPrice:number | undefined = 0;
-    orders.map(order=>{
-      totalPrice = order.products?.reduce((total, item)=> total + (item.quantity! * item.product?.price!), 0)
-    })
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const orders = await getMyOrders(userId)
+  // let totalPrice = 0;
+  // orders.map(order=>{
+  //   totalPrice = order.products?.reduce((total, item)=> total + (item.quantity! * item.product?.price!), 0)
+  // })
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
-        <div className="bg-white p-4 sm:p-8 rounded-xl shadow lg w-full max-w-4xl">
-           <h1 className="text-4xl font-bold text-gray-900 tracking-tight mb-8">Mes commandes</h1>
-           {
-            orders.length === 0 ?(
-              <div className="text-center text-gray-600">
-                <p>Aucun historique de commandes</p>
-              </div>
-            ):(
-              <div className="space-y-6 sm:space-y-8">
-                {
-                  orders.map((order) => (
-                    <div 
-                      key={order.orderNumber}
-                      className="bg-white border-gray-200 rounded-lg shadow-sm overflow-hidden">
-                      <div className="p-4 sm:p-6 border-b border-gray-200">
-                        <div className="flex flex-col gap-4 sm:flex-row sm:justify-between sm:items-center mb-4">
-                          <div>
-                            <p className="text-sm text-gray-600 mb-1 font-bold">
-                              N° de commande:
-                            </p>
-                            <p className="font-mono text-sm text-bold break-all text-gold">
-                              {order.orderNumber}
-                            </p>
-                          </div>
-                          <div className="sm:text-right">
-                            <p className="text-sm text-gray-600 mb-1">Date de la commande:</p>
-                            <p className="font-medium">
-                              {order.orderDate ? new Date(order.orderDate).toLocaleDateString() : "N/A"}
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-
-
-                      <div className="flex flex-col gap-4 sm:flex-row sm:justify-between sm:items-center p-4">
-                        <div className="flex items-center">
-                          <span className="text-sm mr-2">Statut:</span>
-                          <span className={`px-3 py-1 rounded-full text-sm ${order.status === "paid" ? "bg-gold text-white" : "bg-gray-100 text-gray-800"}`}>{order.status}</span>
-                        </div>
-                        <div className="sm:text-right">
-                          <p className="text-sm text-gray-600 mb-1">Montant total:</p>
-                          <p className="font-bold text-lg">
-                            {
-                              order.products?.reduce((total, item) => {
-                                const quantity = item.quantity || 0;
-                                const price = item.product?.price || 0;
-                                return total + quantity * price;
-                              }, 0).toLocaleString()
-                            } FCFA
-                          </p>
-                        </div>
-
-                        {
-                        order.amountDiscount ? (
-                          <div className="mt-4 p-3 sm:p-4 bg-red-50 rounded-lg">
-                            <p className="text-red-600 font-medium mb-1 text-sm sm:text-base">
-                              Réduction appliquée: {" "} {order.amountDiscount} FCFA
-                            </p>
-                            <p className="text-sm text-gray-600">
-                              Prix original: {" "} {
-                              order.products?.map((product)=>(
-                                <span key={product.product?._id}>
-                                  {
-                                    (product.quantity! * product.product?.price!) + order.amountDiscount!
-                                  } FCFA
-                                </span>
-                              ))
-                            }
-                            </p>
-                          </div>
-                        ) : null}
-
-                        
-                      </div>  
-                      <div className="px-4 py-3 sm:px-6 sm:py-4">
-                          <p className="text-sm font-semibold text-gray-600 mb-3 sm:mb-4">
-                            Liste des articles commandés
-                          </p>
-                          <div className="space-y-3 sm:space-y-4">
-                            {
-                              order.products?.map((product)=>(
-                                <div
-                                  key={product.product?._id}
-                                className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3 py-2 border-b last:border-b-0">
-                                  <div className="flex ietms-center gap-3 sm:gap-4">
-                                    {
-                                      product.product?.image && (
-                                        <div className="relative h-14 w-14 sm:h-16 sm:w-16 flex-shrink-0 rounded-md overflow-hidden">
-                                          <Image 
-                                            src={imageUrl(product.product.image).url()}
-                                            alt={product.product.name ?? ""}
-                                            fill
-                                            className="object-cover"
-                                            />
-                                        </div>
-                                      )
-                                    }
-                                    <div>
-                                      <p className="font-medium text-sm sm:text-base">
-                                        {product.product?.name}
-                                      </p>
-                                      <p className="text-sm text-graay-600">
-                                        Qté: {product.quantity ?? "N/A"}
-                                      </p>
-                                    </div>
-                                    
-                                  </div>
-                                  <p className="font-medium text-right">
-                                      {
-                                        product.product?.price && product.quantity ?
-                                        `${product.product?.price * product.quantity} FCFA` : "N/A"
-                                      }
-                                    </p>
-                                </div>
-                              ))
-                            }
-                          </div>
-                        </div>                      
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4'>
+      <div className='bg-white p-4 sm:p-8 rounded-xl shadow lg w-full max-w-4xl'>
+        <h1 className='text-4xl font-bold text-gray-900 tracking-tight mb-8'>
+          Mes commandes
+        </h1>
+        {orders.length === 0 ? (
+          <div className='text-center text-gray-600'>
+            <p>Aucun historique de commandes</p>
+          </div>
+        ) : (
+          <div className='space-y-6 sm:space-y-8'>
+            {orders.map((order) => (
+              <div
+                key={order.orderNumber}
+                className='bg-white border-gray-200 rounded-lg shadow-sm overflow-hidden'
+              >
+                <div className='p-4 sm:p-6 border-b border-gray-200'>
+                  <div className='flex flex-col gap-4 sm:flex-row sm:justify-between sm:items-center mb-4'>
+                    <div>
+                      <p className='text-sm text-gray-600 mb-1 font-bold'>
+                        N° de commande:
+                      </p>
+                      <p className='font-mono text-sm text-bold break-all text-gold'>
+                        {order.orderNumber}
+                      </p>
                     </div>
-                    
-                  ))}
+                    <div className='sm:text-right'>
+                      <p className='text-sm text-gray-600 mb-1'>
+                        Date de la commande:
+                      </p>
+                      <p className='font-medium'>
+                        {order.orderDate
+                          ? new Date(order.orderDate).toLocaleDateString()
+                          : 'N/A'}
+                      </p>
+                    </div>
+                  </div>
+                </div>
 
+                <div className='flex flex-col gap-4 sm:flex-row sm:justify-between sm:items-center p-4'>
+                  <div className='flex items-center'>
+                    <span className='text-sm mr-2'>Statut:</span>
+                    <span
+                      className={`px-3 py-1 rounded-full text-sm ${order.status === 'paid' ? 'bg-gold text-white' : 'bg-gray-100 text-gray-800'}`}
+                    >
+                      {order.status}
+                    </span>
+                  </div>
+                  <div className='sm:text-right'>
+                    <p className='text-sm text-gray-600 mb-1'>Montant total:</p>
+                    <p className='font-bold text-lg'>
+                      {order.products
+                        ?.reduce((total, item) => {
+                          const quantity = item.quantity || 0
+                          const price = item.product?.price || 0
+                          return total + quantity * price
+                        }, 0)
+                        .toLocaleString()}{' '}
+                      FCFA
+                    </p>
+                  </div>
+
+                  {order.amountDiscount ? (
+                    <div className='mt-4 p-3 sm:p-4 bg-red-50 rounded-lg'>
+                      <p className='text-red-600 font-medium mb-1 text-sm sm:text-base'>
+                        Réduction appliquée: {order.amountDiscount} FCFA
+                      </p>
+                      <p className='text-sm text-gray-600'>
+                        Prix original:{' '}
+                        {order.products?.map((product) => (
+                          <span key={product.product?._id}>
+                            {product.product && product.product.price != null
+                              ? product.quantity! * product.product.price +
+                                order.amountDiscount!
+                              : 'N/A'}{' '}
+                            FCFA
+                          </span>
+                        ))}
+                      </p>
+                    </div>
+                  ) : null}
+                </div>
+                <div className='px-4 py-3 sm:px-6 sm:py-4'>
+                  <p className='text-sm font-semibold text-gray-600 mb-3 sm:mb-4'>
+                    Liste des articles commandés
+                  </p>
+                  <div className='space-y-3 sm:space-y-4'>
+                    {order.products?.map((product) => (
+                      <div
+                        key={product.product?._id}
+                        className='flex flex-col sm:flex-row sm:items-center sm:justify-between gap-3 py-2 border-b last:border-b-0'
+                      >
+                        <div className='flex ietms-center gap-3 sm:gap-4'>
+                          {product.product?.image && (
+                            <div className='relative h-14 w-14 sm:h-16 sm:w-16 flex-shrink-0 rounded-md overflow-hidden'>
+                              <Image
+                                src={imageUrl(product.product.image).url()}
+                                alt={product.product.name ?? ''}
+                                fill
+                                className='object-cover'
+                              />
+                            </div>
+                          )}
+                          <div>
+                            <p className='font-medium text-sm sm:text-base'>
+                              {product.product?.name}
+                            </p>
+                            <p className='text-sm text-graay-600'>
+                              Qté: {product.quantity ?? 'N/A'}
+                            </p>
+                          </div>
+                        </div>
+                        <p className='font-medium text-right'>
+                          {product.product?.price && product.quantity
+                            ? `${product.product?.price * product.quantity} FCFA`
+                            : 'N/A'}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
-            )}
-           
-        </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,63 +1,72 @@
-"use client";
+'use client'
 
-import { ClerkLoaded, SignedIn, SignInButton, UserButton, useUser, useSession } from "@clerk/nextjs";
-import Link from "next/link";
-import Form from "next/form";
-import { PackageIcon, TrolleyIcon } from "@sanity/icons";
-import useBasketStore from "@/sanity/lib/store/store";
-import Image from "next/image";
+import {
+  ClerkLoaded,
+  SignedIn,
+  SignInButton,
+  UserButton,
+  useUser,
+  useSession,
+} from '@clerk/nextjs'
+import Link from 'next/link'
+import Form from 'next/form'
+import { PackageIcon, TrolleyIcon } from '@sanity/icons'
+import useBasketStore from '@/sanity/lib/store/store'
+import Image from 'next/image'
 
 function Header() {
-    const { user } = useUser();
-    const { session } = useSession();
+  const { user } = useUser()
+  const { session } = useSession()
 
-    const itemCount = useBasketStore((state)=>
-        state.items.reduce((total, item) => total + item.quantity, 0)
-    );
+  const itemCount = useBasketStore((state) =>
+    state.items.reduce((total, item) => total + item.quantity, 0)
+  )
 
-    const createClerkPasskey = async() => {
-
-        if(!session)
-        {
-            alert('La session a expiré');
-            return
-        }
-        try {
-            const response = await user?.createPasskey()
-            console.log(response)
-        } catch (error) {
-            console.error("Error:", JSON.stringify(error, null, 2))
-        }
+  const createClerkPasskey = async () => {
+    if (!session) {
+      alert('La session a expiré')
+      return
     }
+    try {
+      const response = await user?.createPasskey()
+      console.log(response)
+    } catch (error) {
+      console.error('Error:', JSON.stringify(error, null, 2))
+    }
+  }
   return (
-    <header className="flex flex-wrap justify-between items-center px-4 py-2">
-        <div className="flex w-full flex-wrap justify-between items-center">
-            <Link href="/" 
-            className="
+    <header className='flex flex-wrap justify-between items-center px-4 py-2'>
+      <div className='flex w-full flex-wrap justify-between items-center'>
+        <Link
+          href='/'
+          className='
             text-2xl
             font-bold
             text-gold
             hover:opacity-50
             cursor-pointer
             mx-auto sm:mx-0
-            "
-            >
-                <Image 
-                    src="/moung_shop.png"
-                    alt="MoungShop Fashion"
-                    width={160}
-                    height={40}
-                    className="h-20 w-auto"
-                    priority
-                />
-            </Link>
+            '
+        >
+          <Image
+            src='/moung_shop.png'
+            alt='MoungShop Fashion'
+            width={160}
+            height={40}
+            className='h-20 w-auto'
+            priority
+          />
+        </Link>
 
-            <Form 
-                action="/rechercher"
-                className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
-                >
-                <input type="text" name="query" placeholder="Rechercher un article..."
-                    className="
+        <Form
+          action='/rechercher'
+          className='w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0'
+        >
+          <input
+            type='text'
+            name='query'
+            placeholder='Rechercher un article...'
+            className='
                     bg-gray-100
                     text-gray-800
                     px-4
@@ -70,63 +79,66 @@ function Header() {
                     border
                     w-full
                     max-w-4xl
-                    "
-                />
-            </Form>
+                    '
+          />
+        </Form>
 
-            <div className="flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none">
-                <Link href="/panier"
-                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-gold hover:bg-gold-hover text-white font-bold py-2 px-4 rounded"
-                >
-                    <TrolleyIcon className="w-6 h-6"/>
+        <div className='flex items-center space-x-4 mt-4 sm:mt-0 flex-1 sm:flex-none'>
+          <Link
+            href='/panier'
+            className='flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-gold hover:bg-gold-hover text-white font-bold py-2 px-4 rounded'
+          >
+            <TrolleyIcon className='w-6 h-6' />
 
-                    {/* affichage du nombre d'articles présents dans le panier */}
+            {/* affichage du nombre d'articles présents dans le panier */}
 
-                    <span className="absolute -top-2 -right-2 bg-red-500 text-white
+            <span
+              className='absolute -top-2 -right-2 bg-red-500 text-white
                         rounded-full w-5 h-5 flex items-center justify-center text-xs
-                    ">
-                        { itemCount }
-                    </span>
-                    <span>Mon panier</span>
+                    '
+            >
+              {itemCount}
+            </span>
+            <span>Mon panier</span>
+          </Link>
+
+          {/* Partie utilisateur */}
+          <ClerkLoaded>
+            <SignedIn>
+              {user && (
+                <Link
+                  href='/commandes'
+                  className='flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-black text-white font-bold py-2 px-4 rounded'
+                >
+                  <PackageIcon className='w-6 h-6' />
+                  <span>Mes commandes</span>
                 </Link>
-            
+              )}
+            </SignedIn>
 
-                {/* Partie utilisateur */}
-                <ClerkLoaded>
-                    <SignedIn>
-                        {user && (
-                            <Link 
-                                href="/commandes"
-                                className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-black text-white font-bold py-2 px-4 rounded"
-                                >
-                                <PackageIcon className="w-6 h-6"/>
-                                <span>Mes commandes</span>
-                            </Link>
-                        )}
-                    </SignedIn>
+            {user ? (
+              <div className='flex items-center space-x-2'>
+                <UserButton />
 
-                    {user ? (
-                        <div className="flex items-center space-x-2">
-                            <UserButton />
-
-                            <div className="hidden sm:block text-xs">
-                                <p className="text-gray-400">Bienvenu(e)</p>
-                                <p className="font-bold">{ user.fullName }</p>
-                            </div>
-                        </div>
-                    ): (
-                        <SignInButton mode="modal">
-                            <button className="rounded px-4 py-2 font-semibold text-black border-2 border-black
-                            hover:bg-black hover:text-white transition duration-200 cursor-pointer">
-                                Se connecter
-                            </button>
-                        </SignInButton>
-                    )}
-                    {
-                        user?.passkeys.length === 0 && (
-                            <button 
-                                onClick={createClerkPasskey}
-                                className="
+                <div className='hidden sm:block text-xs'>
+                  <p className='text-gray-400'>Bienvenu(e)</p>
+                  <p className='font-bold'>{user.fullName}</p>
+                </div>
+              </div>
+            ) : (
+              <SignInButton mode='modal'>
+                <button
+                  className='rounded px-4 py-2 font-semibold text-black border-2 border-black
+                            hover:bg-black hover:text-white transition duration-200 cursor-pointer'
+                >
+                  Se connecter
+                </button>
+              </SignInButton>
+            )}
+            {user?.passkeys.length === 0 && (
+              <button
+                onClick={createClerkPasskey}
+                className='
                                 bg-white
                                 hover:bg-gold-hover
                                 hover:text-white
@@ -138,14 +150,14 @@ function Header() {
                                 border-gold
                                 border
                                 cursor-pointer
-                                " 
-                                >Générer clé d'accès</button>
-                        )
-                    }
-                    
-                </ClerkLoaded>
-            </div>
+                                '
+              >
+                Générer clé d{"'"}accès
+              </button>
+            )}
+          </ClerkLoaded>
         </div>
+      </div>
     </header>
   )
 }
